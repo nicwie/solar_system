add5591795e5251800e86875494a91d877cc4fdf
nicwie <nic.wiesinger@yahoo.com>
2025-04-15 15:10:14 +0200

    this should make a triangle, although something currently isn't working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # solar_system
+
+To build, `cd` into `build` and enter `cmake ..`.
+This requires glew, opengl, cmake, and glfw.
+after that, enter `make` and then you should see an executable.
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
 #include <GL/glew.h>
 #include <GL/gl.h>
@@ -9,7 +8,23 @@
 
 #include <glm/glm.hpp>
 
-using namespace glm;
+
+// This is our vertex shader: only passes the data over, but is needed in Opengl
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+// Same for our fragment shader
+const char *fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\0";
+
 
 /**
  * @brief  Checks what keys were pressed and decides what to do with them
@@ -42,7 +57,9 @@ int main(void) {
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Just for MacOS
+#endif
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open the window itself
@@ -55,7 +72,6 @@ int main(void) {
 	}
 	glfwMakeContextCurrent(window);
 
-	glewExperimental = true;
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -67,9 +83,6 @@ int main(void) {
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// to be able to capture press <esc>
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
 	// Background color (here , Black)
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
@@ -79,12 +92,97 @@ int main(void) {
 	// Accept if closer to camer than other
 	glDepthFunc(GL_LESS);
 
-	// main drawing loop 
+
+
+	// This actually compiles the vertex shader we defined above
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// This is for compile time errors in shader compilation
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if(!success) {
+	    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+	    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if(!success) {
+	    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+	    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success) {
+	    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+	    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	glUseProgram(shaderProgram);
+
+	// Now that we have compiled and linked, we do not need the shaders anymore
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);  
+
+	// test triangle	
+	float vertices[] = {
+	    -0.5f, -0.5f, 0.0f,
+	     0.5f, -0.5f, 0.0f,
+	     0.0f,  0.5f, 0.0f
+	};  
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0); 
+
+
+	// uncomment this call to draw in wireframe polygons.
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// main drawing loop
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -100,6 +198,11 @@ int main(void) {
 	}  while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
 	  	glfwWindowShouldClose(window) == 0);
 	*/
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	// close window, terminate GLFW
 	glfwTerminate();
