fa94f37e8aacf596de388be2cddba2d59b077fe5
nicwie <nic.wiesinger@yahoo.com>
2025-05-12 16:23:54 +0200

    Added movable camera and moving

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-
 #include <cmath>
 #include <glm/ext/matrix_clip_space.hpp>
 #include <glm/ext/matrix_float4x4.hpp>
 #include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/quaternion_geometric.hpp>
 #include <glm/ext/vector_float3.hpp>
+#include <glm/geometric.hpp>
 #include <glm/trigonometric.hpp>
 #include <iostream>
 
@@ -18,11 +19,26 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "Camera.hpp"
 #include "Shader.hpp"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 float mixVal = 0.2f;
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing 
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 /**
  * @brief This helper function prints only if there is an error; it is useful since by default, openGL only gives error codes
@@ -59,6 +75,14 @@ GLenum glCheckError_(const char *file, int line) {
 void processInput(GLFWwindow *window) {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 
     // We need to do all of this because we want edge detection: We don't need flickering if the user holds space
     static bool spacePressedLastFrame = false;
@@ -94,6 +118,29 @@ void processInput(GLFWwindow *window) {
     downKeyLastFrame = downKeyThisFrame;
 }
 
+void mouse_callback(GLFWwindow *window, double xposIn, double yposIn) {
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed, y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovements(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
 /**
  * @brief Function to call when the window size changes so that our vieport keeps the correct size
  *
@@ -106,6 +153,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 }
 
 int main(void) {
+
     if (!glfwInit()) {
         fprintf(stderr, "Failed to initalize GLFW\n");
         getchar();
@@ -120,14 +168,20 @@ int main(void) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Open the window itself
-    GLFWwindow* window = glfwCreateWindow(800, 600,  "Solar System", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,  "Solar System", NULL, NULL);
     if (window == NULL) {
         fprintf(stderr, "Failed to open GLFW window.");
         getchar();
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
@@ -167,7 +221,7 @@ int main(void) {
     glBindTexture(GL_TEXTURE_2D, texture2);
 
 
-    // This is because openGL expects 0.0 to be on the bottom of the img, but it is usually on top.
+    // This is done because openGL expects 0.0 to be on the bottom of the img, but it is usually on top.
     stbi_set_flip_vertically_on_load(true);
 
     data = stbi_load("../awesomeface.png", &width, &height, &nrChannels, 0);
@@ -185,7 +239,7 @@ int main(void) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
@@ -199,7 +253,7 @@ int main(void) {
 
     // this triangle has colors as well
     float verticesTri[] = {
-        // position          // colors           // texture coordinates
+        // position          // texture coordinates
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
          0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
          0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
@@ -290,12 +344,17 @@ int main(void) {
     // uncomment this call to draw in wireframe polygons.
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+
     // main drawing loop
     while (!glfwWindowShouldClose(window)) {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         processInput(window);
 
         // Here we define our matrices for model, view, and projection
-
+        // view = glm::mat4(1.0f);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -306,30 +365,26 @@ int main(void) {
         glCheckError();
         glBindVertexArray(VAOTri);
         glCheckError();
+
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        Tri.setMat4("projection", projection);
+
+        glm::mat4 view = camera.GetViewMatrix();
+        Tri.setMat4("view", view);
+
         for (int i = 0; i < 10; i++) {
             glm::mat4 model = glm::mat4(1.0f);
             // Note that we translate by -z -> away from us
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
-            model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            if (i % 3 == 0) {
+            if (i % 1 == 0) {
                 model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
             }
 
 
             Tri.setMat4("model", model);
 
-            glm::mat4 view = glm::mat4(1.0f);
-
-            glm::mat4 projection;
-            projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
-            GLint modelLoc = glGetUniformLocation(Tri.ID, "model");
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-            GLint viewLoc = glGetUniformLocation(Tri.ID, "view");
-            glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-            GLint projLoc = glGetUniformLocation(Tri.ID, "projection");
-            glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
             glDrawArrays(GL_TRIANGLES, 0, 36);
             glCheckError();
         }
